2dbacdc55e4d4b0bc1ba3b6db1e2521598b4c777
Metin Ahmad <metinmayi@gmail.com>
2022-08-08 14:11:00 +0200

    Fixed delete

--- javascript/allBlogsPage/allBlogs.js
+++ javascript/allBlogsPage/allBlogs.js
@@ -1,6 +1,5 @@
 import { toggleHamburger } from "../utilities/toggleHamburger.js";
 toggleHamburger();
-let i = 0;
 const divsBlog = document.getElementById("divs-blog");
 // Hämtar vår lista av blogs från sessionStorage
 let blogs = JSON.parse(sessionStorage.getItem("blogs"));
@@ -11,37 +10,32 @@ if (!blogs) {
   title.innerText = "No saved blogposts";
   divsBlog.append(title);
 } else {
-
   // Om vi har en lista av blogs, gå igenom den lista och skapa HTML element av dem
-  for ( i = 0; i<blogs.length; i++) {
+  for (let i = 0; i < blogs.length; i++) {
     // Skapa en div som innehåller headline(h3), author(h6), blogText(p).
     // Värdena på dessa HTML-taggar ska komma från objektet som vi går igenom i loopen.
     // Lägg till den nya diven i divsBlog.
-    const container = document.createElement("div"); container.setAttribute('id', blogs[i]);
+    const container = document.createElement("div");
+    container.setAttribute("id", blogs[i]);
     const headline = document.createElement("h3");
     const author = document.createElement("h6");
     const blogText = document.createElement("p");
-    const deleteBtn = document.createElement("button"); deleteBtn.setAttribute('id', blogs[i]);
-    const editBtn = document.createElement("button"); editBtn.setAttribute('id', blogs[i]); 
+    const deleteBtn = document.createElement("button");
+    const editBtn = document.createElement("button");
+    editBtn.setAttribute("id", blogs[i]);
 
     blogText.innerText = blogs[i].blogText;
     author.innerText = blogs[i].author;
     headline.innerText = blogs[i].headline;
-    deleteBtn.innerText="Delete";
-    editBtn.innerText="Edit";
+    deleteBtn.innerText = "Delete";
+    editBtn.innerText = "Edit";
     container.append(headline, author, blogText, deleteBtn, editBtn);
     divsBlog.append(container);
-    
-    deleteBtn.addEventListener('click', () => {
-      if(deleteBtn.id === container.id ){
-       container.remove();
-       blogs.splice(blogs - blogs[i]);    
-       sessionStorage.setItem("blogs", JSON.stringify(blogs));
-       blogs = JSON.parse(sessionStorage.getItem("blogs"));
-      }
-      });
-      
+
+    deleteBtn.addEventListener("click", () => {
+      container.remove();
+      blogs.splice(i, i);
+      sessionStorage.setItem("blogs", JSON.stringify(blogs));
+    });
   }
-  
 }
-console.log(blogs);
